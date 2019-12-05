d77d435e42d6574ddd53263665b8291fe5404e4c
peyman <peymanahmadifar@yahoo.com>
2019-12-05 16:46:27 +0330

    adds url of web app to project - adds new view - adds Token model and migrations

--- web/models.py
+++ web/models.py
@@ -2,7 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=128)
+    def __str__(self):
+        return self.user.username
+
 
 class Expense(models.Model):
     text = models.CharField(max_length=300)
---
+++ web/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('submit/expense', views.submit_expense, name='submit_expense')
+]
